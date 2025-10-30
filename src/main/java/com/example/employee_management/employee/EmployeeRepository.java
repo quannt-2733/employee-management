7c898be23362f87b595f77c5226cdff8c2f6e017
quannt-2733 <nguyen.tien.quan@sun-asterisk.com>
2025-10-30 17:13:07 +0700

    module: reporting and analytics

--- src/main/java/com/example/employee_management/employee/EmployeeRepository.java
+++ src/main/java/com/example/employee_management/employee/EmployeeRepository.java
@@ -1,13 +1,24 @@
 package com.example.employee_management.employee;
 
+import com.example.employee_management.dto.DepartmentStatisticDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // Spring Data JPA auto generate query: SELECT * FROM employees WHERE name LIKE '%<name>%'
     List<Employee> findByNameContainingIgnoreCase(String name);
 
     // Query: SELECT * FROM employees WHERE department_id = <departmentId>
     List<Employee> findByDepartmentId(Long departmentId);
+
+    @Query("SELECT new com.example.employee_management.dto.DepartmentStatisticDTO(e.department.name, COUNT(e.id)) " +
+            "FROM Employee e " +
+            "WHERE e.department IS NOT NULL " +
+            "GROUP BY e.department.name " +
+            "ORDER BY COUNT(e.id) DESC")
+    List<DepartmentStatisticDTO> countEmployeesByDepartment();
 }
